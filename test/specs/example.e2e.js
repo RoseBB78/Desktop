18c7a03208ab09a05f11ed3f13680ed923fa6fe6
RoseBB78 <rosemary.ballestero.barquero@ucreativa.com>
2021-11-20 21:44:48 -0600

    https://github.com/RoseBB78/RepoCICD.git

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,10 +1,24 @@
 import LoginPage from  '../pageobjects/login.page';
 import AuthPage from  '../pageobjects/auth.page';
+import DropdownPage from '../pageobjects/auth.page';
+import InputsPage from '../pageobjects/auth.page';
 
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
         await LoginPage.login('admin', 'admin');
         await expect(AuthPage.getTitle).toBeExisting();
         await expect(AuthPage.getTitle).toHaveTextContaining('Basic Auth');
+    });
+});
+describe('My Dropdown App',() =>{
+    it('Should open the dropdown app',async() =>{
+        await DropdownPage.open('drowpdown');
+        await expect(DropdownPage.getTitle).toHaveTextContaining('Drowpdown List');
+    });
+});
+describe('Inputs',() =>{
+    it('Should open the inputs',async() =>{
+        await InputsPage.open('inputs');
+        await expect(InputsPage.getTitle).toHaveTextContaining('Inputs');
     });
 });
